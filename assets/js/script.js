9b4eae53568098422d74e6766b33b0b5cd211668
Carina Reyes <59748133+Creyes17e@users.noreply.github.com>
2020-05-12 21:49:33 -0500

    created function that calls weather information after clicking city btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,53 @@
 //Initial cities on page
 var cities = ["Houston", "Atlanta", "Austin", "Dallas"];
+var currentDate = new Date().toDateString();
 
-//function to display city data
+//function to display city weather
+function displayCityWeather() {
+  var APIKey = "16579f7bf39afcac0733e6f89e871775";
+  var city = $(this).attr("data-name");
+  var queryUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey;
+  console.log(queryUrl);
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+  }).then(function (response) {
+    //div to hold city weather
+    $("#display-city-weather").empty();
+    var cityDiv = $("#display-city-weather");
+    var cityNameP = $("<p>").html(
+      "<h2>" + response.name + "-" + `(${currentDate})</h2>`
+    );
+    cityDiv.append(cityNameP);
+
+    var tempEl = ((response.main.temp - 273.15) * 1.8 + 32).toFixed(1);
+    var tempP = $("<p>").text("Temperature:" + tempEl);
+    cityDiv.append(tempP);
+    console.log(tempEl);
+
+    var humidityEl = response.main.humidity;
+    var humidityP = $("<p>").text("Humidity:" + humidityEl);
+    cityDiv.append(humidityP);
+    console.log(humidityEl);
+
+    var windEl = response.wind.speed;
+    var windP = $("<p>").text("Wind Speed:" + windEl);
+    cityDiv.append(windP);
+    console.log(windEl);
+  });
+}
+
+//Function to display city btns
 function renderCityBtns() {
   $("#city-btns-view").empty();
 
   for (var i = 0; i < cities.length; i++) {
-    var cityBtn = $("<button class='city'>");
+    var cityBtn = $("<button class='city-btn'>");
     cityBtn.attr("data-name", cities[i]);
-    cityBtn.attr("display", "table");
     cityBtn.text(cities[i]);
     $("#city-btns-view").prepend(cityBtn);
   }
@@ -22,25 +61,5 @@ $("#add-city-btn").on("click", function (event) {
   cities.push(city);
   renderCityBtns();
 });
+$(document).on("click", ".city-btn", displayCityWeather);
 renderCityBtns();
-//function to display cities
-function displayCities() {
-  var city = $(this).attr("data-name");
-  var url =
-    "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=" +
-    city +
-    "&appid=" +
-    APIKey;
-  var APIKey = "16579f7bf39afcac0733e6f89e871775";
-  $.ajax({
-    url,
-    method: "GET",
-  }).then(function (response) {
-    //div to hold cities
-    var cityDiv = $("<div class='city'>");
-    //storing
-
-    console.log(url);
-  });
-}
