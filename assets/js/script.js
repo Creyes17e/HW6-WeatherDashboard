8799d24500c065cb94fcaf01c8cb20896c18cddf
Carina Reyes <59748133+Creyes17e@users.noreply.github.com>
2020-05-13 00:38:11 -0500

    on click of button current weather & five day temp and humiditydisplay

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 var cities = ["Houston", "Atlanta", "Austin", "Dallas"];
 var currentDate = new Date().toDateString();
 
-//function to display city weather
+//Function to display city weather
 function displayCityWeather() {
   var APIKey = "16579f7bf39afcac0733e6f89e871775";
   var city = $(this).attr("data-name");
@@ -11,33 +11,76 @@ function displayCityWeather() {
     city +
     "&appid=" +
     APIKey;
-  console.log(queryUrl);
+
   $.ajax({
     url: queryUrl,
     method: "GET",
   }).then(function (response) {
-    //div to hold city weather
     $("#display-city-weather").empty();
+    //Div to hold city weather
     var cityDiv = $("#display-city-weather");
+    //City Naem
     var cityNameP = $("<p>").html(
       "<h2>" + response.name + "-" + `(${currentDate})</h2>`
     );
     cityDiv.append(cityNameP);
-
+    //City Temp in Farenheit
     var tempEl = ((response.main.temp - 273.15) * 1.8 + 32).toFixed(1);
-    var tempP = $("<p>").text("Temperature:" + tempEl);
+    var tempP = $("<p>").text("Temperature:" + tempEl + "F");
     cityDiv.append(tempP);
-    console.log(tempEl);
 
+    //City Humidity
     var humidityEl = response.main.humidity;
-    var humidityP = $("<p>").text("Humidity:" + humidityEl);
+    var humidityP = $("<p>").text("Humidity:" + humidityEl + "%");
     cityDiv.append(humidityP);
-    console.log(humidityEl);
 
+    //City Wind Speed
     var windEl = response.wind.speed;
-    var windP = $("<p>").text("Wind Speed:" + windEl);
+    var windP = $("<p>").text("Wind Speed:" + windEl + "MPH");
     cityDiv.append(windP);
-    console.log(windEl);
+  });
+
+  //Display 5-day Forecast
+  var APIKey = "16579f7bf39afcac0733e6f89e871775";
+  var city = $(this).attr("data-name");
+  console.log(city);
+  var queryUrl2 =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=" +
+    APIKey;
+  console.log(queryUrl2);
+
+  $.ajax({
+    url: queryUrl2,
+    method: "GET",
+  }).then(function (response) {
+    $("#display-city-forecast").empty();
+    var weatherResults = response.list;
+    for (var i = 0; i < weatherResults.length; i += 8) {
+      var displayCityForecastDiv = $("#display-city-forecast");
+      // var date = weatherResults[i].dt_text;
+      //date does not display, need to fix
+      var tempEl2 = ((weatherResults[i].main.temp - 273.15) * 1.8 + 32).toFixed(
+        1
+      );
+      var humidityEl2 = weatherResults[i].main.humidity;
+      console.log(tempEl2);
+
+      // var displayDate = $("<p class='card-title'>").text(setDate);
+      var displayTemp = $("<p class='card-text'>").text(
+        "Temp:" + tempEl2 + "F"
+      );
+      var displayHumidity = $("<p class='card-text'>").text(
+        "Humidity:" + humidityEl2 + "%"
+      );
+      // var weather = weatherResults[i].weather[0].main;
+
+      // displayCityForecastDiv.append(displayDate);
+      displayCityForecastDiv.append(displayTemp);
+      displayCityForecastDiv.append(displayHumidity);
+      $("#display-city-forecast").append(displayCityForecastDiv);
+    }
   });
 }
 
@@ -62,4 +105,5 @@ $("#add-city-btn").on("click", function (event) {
   renderCityBtns();
 });
 $(document).on("click", ".city-btn", displayCityWeather);
+
 renderCityBtns();
