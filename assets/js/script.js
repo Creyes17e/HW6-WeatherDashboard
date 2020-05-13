e13f2ec7c0ff8337e0fb600b823e8bab33e4c313
Carina Reyes <59748133+Creyes17e@users.noreply.github.com>
2020-05-12 20:27:19 -0500

    created a function to render city btns and display them on the html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,46 @@
-var cities = [""];
+//Initial cities on page
+var cities = ["Houston", "Atlanta", "Austin", "Dallas"];
 
-function displayCities() {
-  var city = $(this).attr("data-name");
-  var APIKey = "16579f7bf39afcac0733e6f89e871775";
-  var url =
-    "https://api.openweathermap.org/data/2.5/weather?" +
-    "q=Houston&appid=" +
-    APIKey;
+//function to display city data
+function renderCityBtns() {
+  $("#city-btns-view").empty();
+
+  for (var i = 0; i < cities.length; i++) {
+    var cityBtn = $("<button class='city'>");
+    cityBtn.attr("data-name", cities[i]);
+    cityBtn.attr("display", "table");
+    cityBtn.text(cities[i]);
+    $("#city-btns-view").prepend(cityBtn);
+  }
 }
 
-$.ajax({
-  url,
-  method: "GET",
-}).then(function (response) {
-  console.log(response);
-  console.log(queryURL);
+//Function for event handler for button click
+$("#add-city-btn").on("click", function (event) {
+  event.preventDefault();
+
+  var city = $("#city-input").val().trim();
+  cities.push(city);
+  renderCityBtns();
 });
+renderCityBtns();
+//function to display cities
+function displayCities() {
+  var city = $(this).attr("data-name");
+  var url =
+    "https://api.openweathermap.org/data/2.5/weather?" +
+    "q=" +
+    city +
+    "&appid=" +
+    APIKey;
+  var APIKey = "16579f7bf39afcac0733e6f89e871775";
+  $.ajax({
+    url,
+    method: "GET",
+  }).then(function (response) {
+    //div to hold cities
+    var cityDiv = $("<div class='city'>");
+    //storing
+
+    console.log(url);
+  });
+}
